d10ee7690b2242a4ac0ca58e469e4e320125e69e
Lamont Banks <lamontbanks@gmail.com>
2024-09-26 13:37:28 -0400

    Constants file

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import pygame
 
+from constants import *
 
 
 
 def main():
     print("Starting asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
 
 # Keep at bottom of file
 # This line ensures the main() function is only called when this file is run directly; 
