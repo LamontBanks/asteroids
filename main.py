445c7236d16c2bf242a28d658415d3e412e5f1f6
Lamont Banks <lamontbanks@gmail.com>
2024-09-26 17:22:26 -0400

    Basic game loop

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# Docs: https://www.pygame.org/docs/ref/pygame.html
 import pygame
 
 from constants import *
@@ -8,6 +9,21 @@ def main():
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    # Game loop
+    while (True):
+
+        # Listen for the "close window" event to close the game
+        for event in pygame.event.get():
+            print(event)
+            if (event.type == pygame.QUIT):
+                return
+
+        screen.fill(pygame.Color(0, 0, 0))
+        pygame.display.flip()
 
 # Keep at bottom of file
 # This line ensures the main() function is only called when this file is run directly; 
