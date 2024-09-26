a27ddf23b554235962c48355d711d322cf703b7c
Lamont Banks <lamontbanks@gmail.com>
2024-09-26 13:13:27 -0400

    Boilerplater

---
+++ main.py
@@ -0,0 +1,15 @@
+import pygame
+
+
+
+
+def main():
+    print("Starting asteroids!")
+
+# Keep at bottom of file
+# This line ensures the main() function is only called when this file is run directly; 
+# it won't run if it's imported as a module. 
+
+# It's considered the "pythonic" way to structure an executable program in Python.
+if __name__ == "__main__":
+    main()
